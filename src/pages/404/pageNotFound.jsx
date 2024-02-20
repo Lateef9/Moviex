561f5a21d8cbe2a074e0a6d8695fe1912a68e135
Shaik Lateef <shaiklateef7761@gmail.com>
2024-02-20 23:40:38 +0530

    Routing Configuration

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,9 +1,9 @@
 import "./style.scss"
 
-const pageNotFound = () => {
+const PageNotFound = () => {
   return (
     <div>pageNotFound</div>
   )
 }
 
-export default pageNotFound
+export default PageNotFound
