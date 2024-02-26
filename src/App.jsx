850bf75476c81766ffc0a368b6d85e981c9331ed
Shaik Lateef <shaiklateef7761@gmail.com>
2024-02-26 22:37:57 +0530

    updated Herobanner and fetch api for bg

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   };
 
   return <BrowserRouter>
-
+      {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -37,6 +37,8 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      {/* <Footer /> */}
+
   
   </BrowserRouter>
   ;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,40 @@
 import "./style.scss"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import {useFetch} from "../../../hooks/useFetch"
 
 const HeroBanner = () => {
+
+  // const [Background,setBackground] = useState("");
+  const [Query,setQuery] = useState("");
+  const navigate = useNavigate();
+  const {data,loading} = useFetch("/movie/upcoming")
+
+  const searchQueryHandler =(event) =>{
+    if(event.key === "Enter" && Query.length > 0){
+      navigate(`/search/${Query}`)
+    }
+
+  }
+
   return (
-    <div>HeroBanner</div>
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">Millions of Movies ,TV shows and etc</span>
+          <div className="searchInput">
+            <input 
+            type="text"
+            placeholder="Search for movie"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
